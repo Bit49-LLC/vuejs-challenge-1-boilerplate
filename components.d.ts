58674e5b0702dea87aee15fce973a875e0a162b0
Ellis Madagan <ellismadagan@gmail.com>
2025-05-16 20:23:31 -0400

    chore(deps): npm audit fix

--- components.d.ts
+++ components.d.ts
@@ -7,14 +7,6 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElAvatar: (typeof import('element-plus/es'))['ElAvatar']
-    ElCard: (typeof import('element-plus/es'))['ElCard']
-    ElDescriptions: (typeof import('element-plus/es'))['ElDescriptions']
-    ElDescriptionsItem: (typeof import('element-plus/es'))['ElDescriptionsItem']
-    ElImage: (typeof import('element-plus/es'))['ElImage']
-    ElPagination: (typeof import('element-plus/es'))['ElPagination']
-    ElTableV2: (typeof import('element-plus/es'))['ElTableV2']
-    ElTag: (typeof import('element-plus/es'))['ElTag']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
