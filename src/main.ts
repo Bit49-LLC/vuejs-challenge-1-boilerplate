fd464bc2174fd6ddb25dc3d904b2fdc917e7b872
Ellis Madagan <ellismadagan@gmail.com>
2025-07-11 11:44:03 -0400

    chore: organize main.ts imports

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
-
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
-
 import './assets/main.css'
 
 const app = createApp(App)
